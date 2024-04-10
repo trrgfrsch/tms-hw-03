008315582512a03d8568bd031d26159fd43c87c0
Yana Astashonok <diableivan@gmail.com>
2024-04-10 20:54:48 +0200

    removed playground submodule

---
+++ Cocoa 02.playground/Contents.swift
@@ -0,0 +1,46 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+"lkjkl\"jfghjkg \nhgfhjk,m"
+
+#"Hello, "world!"#
+
+"""
+Hello, world!\
+bye!
+"""
+
+let x = 10
+let y = x + 3
+let z = x + y
+
+var a = 15
+var b = 7
+
+a = b + x
+b = a
+a = a * 2 + z
+
+
+let hello = 5 + 4 * 1 / 2 - 6
+let world = """
+Яна Асташонок 
+22 года
+3 "апреля" 2024 года
+"""
+
+let isFemale = true
+let age = 45
+
+if isFemale == true && age >= 40 {
+    print("give a pension")
+}
+
+let value = 4
+let power = 30
+var result = 1
+
+for i in 1..<30 {
+    result *= value
+    print (i, result)
+}
